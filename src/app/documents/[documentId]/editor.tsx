f1a6076a59161255b5772ab872fb38c07083c621
Ashot Israelyan <ashot.israelyan@tumo.org>
2025-06-11 18:42:44 +0400

    feat: integrate color and link extensions in Tiptap editor, enhance toolbar with image and color selection features

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { Color } from '@tiptap/extension-color';
 import FontFamily from '@tiptap/extension-font-family';
+import Highlight from '@tiptap/extension-highlight';
 import Image from '@tiptap/extension-image';
+import Link from '@tiptap/extension-link';
 import Table from '@tiptap/extension-table';
 import TableCell from '@tiptap/extension-table-cell';
 import TableHeader from '@tiptap/extension-table-header';
@@ -53,6 +56,11 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: 'https',
+      }),
       Table,
       TableCell,
       TableHeader,
@@ -64,6 +72,10 @@ export const Editor = () => {
       Underline,
       TextStyle,
       FontFamily,
+      Color,
+      Highlight.configure({
+        multicolor: true,
+      }),
     ],
     content: '',
   });
