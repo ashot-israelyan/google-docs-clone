95e2ef5be34cd149fc0cf0f757abc800bff28104
Ashot Israelyan <ashot.israelyan@tumo.org>
2025-06-12 16:54:15 +0400

    feat: integrate Navbar component into document page layout, enhance toolbar positioning for improved user experience

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { Navbar } from '@/app/documents/[documentId]/navbar';
+
 import { Editor } from './editor';
 import { Toolbar } from './toolbar';
 
@@ -12,8 +14,13 @@ const DocmentIdPage: FC<DocumentIdPageProps> = async ({ params }) => {
 
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
-      <Toolbar />
-      <Editor />
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
     </div>
   );
 };
